7488832dd556687d859361d0fba6c00f83a01b41
S4ntini <santinieduardo92@gmail.com>
2020-09-02 17:00:14 -0300

    skl

---
+++ Pessoa.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <iostream>
+#include <istream>
+#include <string>
+using namespace std;
+
+class Pessoa {
+private:
+	string name;//ToDo -> declarar os atributos restantes referentes ao obj. pessoa
+protected:
+	Pessoa();
+	void getCustomerData();
+	void putData();
+};
