9bbead98f84a5984b1e8d808ca5a7cc23e907497
S4ntini <santinieduardo92@gmail.com>
2020-09-04 20:08:13 -0300

    pullRequestOrderTest

--- Source.cpp
+++ Source.cpp
@@ -5,7 +5,7 @@ int main() {
 	Menu menuController;
 
 	/*ToDo -> debugar(se necessario) para encaixar o comando 'system("cls");' em alguma parte do loop
-	*o objetivo eh limpar a tela toda vez(check for exceptions) que o user digitar algum comando.. 
+	*o objetivo eh limpar a tela toda vez(check for exceptions) que o user digitar algum comando... 
 	*/
 
 	do {
